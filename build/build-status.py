6e36e46aa3d6782849981bdcdbfe579dfce7e106
Menglei <mengleizhang.mz@gmail.com>
2018-01-15 21:06:58 +0800

    remove files

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['scratch/ns3.26-tcp-wireless-debug', 'scratch/subdir/ns3.26-subdir-debug', 'scratch/ns3.26-mmwave-beamsweeping-toy-example-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
