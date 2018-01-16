1aba85c0f109fdbf8a1ba574becb1db9066ee17e
carhercl <carhercl@iteam.upv.es>
2018-01-16 19:28:39 +0100

    Squashed commit of the following:
    commit 060ebf93bb4792669ee9ba774d3008e9b05aea8b
    Author: Menglei <mengleizhang.mz@gmail.com>
    Date:   Tue Jan 16 08:10:14 2018 +0800
    
    	change speed
    
    commit b2942f2ea50f7d41a786880eb8d1d73ab5e83503
    Author: Menglei <mengleizhang.mz@gmail.com>
    Date:   Mon Jan 15 21:08:55 2018 +0800
    
    	new version
    
    commit 6e36e46aa3d6782849981bdcdbfe579dfce7e106
    Author: Menglei <mengleizhang.mz@gmail.com>
    Date:   Mon Jan 15 21:06:58 2018 +0800
    
    	remove files

--- build/build-status.py
+++ build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['scratch/ns3.26-tcp-wireless-debug', 'scratch/subdir/ns3.26-subdir-debug', 'scratch/ns3.26-mmwave-beamsweeping-toy-example-debug']
+ns3_runnable_programs = ['build/scratch/ns3.26-tcp-wireless-debug', 'build/scratch/subdir/ns3.26-subdir-debug', 'build/scratch/ns3.26-mmwave-beamsweeping-toy-example-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
